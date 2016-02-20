9ca29ec3b011690a0a0d6773094381ed4c4f9974
Evgeny Proydakov <e.proydakov@gmail.com>
2016-02-20 20:25:45 +0300

    Added calc stats

--- letter_freq.go
+++ letter_freq.go
@@ -15,27 +15,40 @@ func check(e error) {
 
 type FreqCalculator struct{
     alphabet []rune
-    dict map[string]int
+    value_dict map[string]int
+    stats_dict map[string]int
 }
 
 func (c *FreqCalculator) InitDict(alphabet []rune) {
     c.alphabet = alphabet
-    c.dict = make(map[string]int)
+    c.value_dict = make(map[string]int)
     for _, letter := range c.alphabet {
         text_letter := string(letter)
         //fmt.Println("Insert:", text_letter)
-        c.dict[text_letter] = 0
+        c.value_dict[text_letter] = 0
     }
 }
 
 func (c *FreqCalculator) Process(r rune) {
     letter := strings.ToLower( string(r) )
 
-    if v, ok := c.dict[letter]; ok {
-        c.dict[letter] = v + 1
+    if v, ok := c.value_dict[letter]; ok {
+        c.value_dict[letter] = v + 1
         //fmt.Printf("Process: '%s'\n", letter)
     } else {
         //fmt.Printf("Skip: '%s'\n", letter)
+    }
+}
+
+func (c *FreqCalculator) CalcStatistic() {
+    counter := 0
+    for _, value := range c.value_dict {
+        counter += value
+    }
+
+    c.stats_dict = make(map[string]int)
+    for key, value := range c.value_dict {
+        c.stats_dict[key] = value * 100.0 / counter
     }
 }
 
@@ -60,12 +73,9 @@ func main() {
     for _, element := range runedata {
         calc.Process(element)
     }
+    calc.CalcStatistic()
 
-    /*
-    for k, v := range calc.dict {
-        fmt.Println("Key:", k, "Value:", v)
+    for k, v := range calc.stats_dict {
+        fmt.Println("Key:", k, "Value:", v, "%")
     }
-    fmt.Printf("InitDict: %v\n", len(calc.dict))
-    */
-    fmt.Println(calc)
 }
